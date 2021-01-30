5fe5d0d2461c46c1c24c3c9923ee3109db60f962
asqwklop12 <asqwklop12@gmail.com>
2021-01-30 14:19:56 +0900

    git init

---
+++ src/main/java/restapiset/RestapisetApplication.java
@@ -0,0 +1,13 @@
+package restapiset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapisetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestapisetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/restapiset/RestapisetApplicationTests.java
@@ -0,0 +1,13 @@
+package restapiset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapisetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
