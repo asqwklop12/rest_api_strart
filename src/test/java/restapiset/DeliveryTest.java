83abb43c7ffcb7ce8d92c6b5cc11d46ebcf8b41d
asqwklop12 <asqwklop12@gmail.com>
2021-02-27 17:32:24 +0900

    db 분리

--- src/test/java/restapiset/DeliveryTest.java
+++ src/test/java/restapiset/DeliveryTest.java
@@ -27,12 +27,14 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
 @Import(RestDocsConfiguration.class)
+@ActiveProfiles("test")
 class DeliveryTest {
 
   @Autowired
